6e471c79b5d2bd67494c0c576297a8581b412e4f
Rafael Carvalho Santos <rafaelalonsobarbosa@gmail.com>
2022-08-12 16:12:06 -0300

    test(Product_Report)
    implementado teste de relatório do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from ..factories.product_factory import ProductFactory
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    productFake = ProductFactory()
+    product = Product(
+        productFake.id,
+        productFake.nome_do_produto,
+        productFake.nome_da_empresa,
+        productFake.data_de_fabricacao,
+        productFake.data_de_validade,
+        productFake.numero_de_serie,
+        productFake.instrucoes_de_armazenamento,
+    ).__repr__()
+
+    assert product == (
+        f"O produto {productFake.nome_do_produto}"
+        f" fabricado em {productFake.data_de_fabricacao}"
+        f" por {productFake.nome_da_empresa} com validade"
+        f" até {productFake.data_de_validade}"
+        f" precisa ser armazenado {productFake.instrucoes_de_armazenamento}."
+    )

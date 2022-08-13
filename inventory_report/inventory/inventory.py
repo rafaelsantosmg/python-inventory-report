fc1caebe10d2e25b5192a8e886c5afbeff4cb6a4
Rafael Carvalho Santos <rafaelalonsobarbosa@gmail.com>
2022-08-13 20:39:14 -0300

    feat(Inventory)
    implementado a classe Inventory import json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,33 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def import_data(file_path, strategy):
+    def import_csv(file_path):
         inventory_list = list()
         with open(file_path, encoding="utf8") as file:
             inventory_reader = csv.DictReader(file)
             for row in inventory_reader:
                 inventory_list.append(dict(row))
+        return inventory_list
+
+    def import_json(file_path):
+        inventory_list = list()
+        with open(file_path) as file:
+            inventory_reader = json.load(file)
+            print(inventory_reader)
+            for row in inventory_reader:
+                inventory_list.append(dict(row))
+        return inventory_list
+
+    def import_data(file_path, strategy):
+        inventory_list = list()
+        if "csv" in file_path:
+            inventory_list = Inventory.import_csv(file_path)
+        elif "json" in file_path:
+            inventory_list = Inventory.import_json(file_path)
 
         if strategy == "simples":
             return SimpleReport.generate(inventory_list)

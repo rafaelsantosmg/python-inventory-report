53491d148aca431b609f4e1392570b54411a839e
Rafael Carvalho Santos <rafaelalonsobarbosa@gmail.com>
2022-08-13 21:27:37 -0300

    feat(Inventory)
    implementado a classe Inventory import xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,33 +1,43 @@
 import csv
 import json
+from xml.etree import ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     def import_csv(file_path):
-        inventory_list = list()
         with open(file_path, encoding="utf8") as file:
-            inventory_reader = csv.DictReader(file)
-            for row in inventory_reader:
-                inventory_list.append(dict(row))
-        return inventory_list
+            inventory_reader = list(csv.DictReader(file))
+        return inventory_reader
 
     def import_json(file_path):
-        inventory_list = list()
         with open(file_path) as file:
-            inventory_reader = json.load(file)
-            print(inventory_reader)
-            for row in inventory_reader:
-                inventory_list.append(dict(row))
-        return inventory_list
+            inventory_reader = list(json.load(file))
+        return inventory_reader
+
+    def import_xml(file_path):
+        tree = ET.parse(file_path)
+        root = list(tree.getroot())
+        dict_list = list()
+        info_dict = dict()
+        for index in range(len(root)):
+            for info in root[index]:
+                info_dict[info.tag] = info.text
+            dict_list.append(info_dict)
+            info_dict = {}
+        return dict_list
+
+    def select_file(file_path):
+        if "csv" in file_path:
+            return Inventory.import_csv(file_path)
+        elif "json" in file_path:
+            return Inventory.import_json(file_path)
+        elif "xml" in file_path:
+            return Inventory.import_xml(file_path)
 
     def import_data(file_path, strategy):
-        inventory_list = list()
-        if "csv" in file_path:
-            inventory_list = Inventory.import_csv(file_path)
-        elif "json" in file_path:
-            inventory_list = Inventory.import_json(file_path)
+        inventory_list = Inventory.select_file(file_path)
 
         if strategy == "simples":
             return SimpleReport.generate(inventory_list)
